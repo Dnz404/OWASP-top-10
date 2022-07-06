ac2b2d72239457bb116e24e580092ccf35c7b947
Dnz404 <letscode010100@gmail.com>
2022-07-06 20:56:40 -0300

    Adicionando informações e exemplos

--- A03-Injection.sql
+++ A03-Injection.sql
@@ -1,2 +1,11 @@
+/* User-supplied data is not validated or sanitizated by the application*/
+/*Hostile data is used via ORM(object-relational mapping) searching for parameters to get sensitive records*/
+/*Intruder data is directly used or concatenated(SQL querries, etc)*/
+
+Example 1:
 String query = "SELECT \* FROM accounts WHERE custID='" + request.getParameter("id") + "'";
+
+Example 2:
+ Query HQLQuery = session.createQuery("FROM accounts WHERE custID='" + request.getParameter("id") + "'");
+
 
